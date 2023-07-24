c6adfdd4acc84a6970daa046b0ba819324f546b4
Rachel Miller <rkmiller131@gmail.com>
2023-07-24 11:10:15 -0700

    Added quick connection to front end - will be dockerized later

--- src/main/java/com/cfVanguardBackend/oldskoolbackend/controllers/CardController.java
+++ src/main/java/com/cfVanguardBackend/oldskoolbackend/controllers/CardController.java
@@ -5,15 +5,16 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cfVanguardBackend.oldskoolbackend.exceptions.CardNotFoundException;
 import com.cfVanguardBackend.oldskoolbackend.models.Card;
 import com.cfVanguardBackend.oldskoolbackend.services.CardService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class CardController {
   // we need a reference to our service class and just like we did in service, we can autowire this in
   @Autowired
@@ -26,12 +27,10 @@ public class CardController {
   }
 
   // get a single card by its name
-  // @GetMapping("/cards/{name}")
-  // public ResponseEntity<Card> getCardByName(@PathVariable String name) {
-  //   return ResponseEntity.of(Optional.ofNullable(cardService.oneCard(name)));
-  // }
   @GetMapping("/cards/{name}")
   public ResponseEntity<Card> getCardByName(@PathVariable String name) {
+    // This commented out return was not producing a 404 if not found: was returning null with 200
+    // return ResponseEntity.of(Optional.ofNullable(cardService.oneCard(name)));
     return ResponseEntity.of((cardService.oneCard(name)));
   }
 }
