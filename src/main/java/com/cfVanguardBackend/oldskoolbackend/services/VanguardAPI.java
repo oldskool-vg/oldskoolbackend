d9e5dd8c25723de1d9f624e55a6eef82fab1f1f2
Rachel Miller <rkmiller131@gmail.com>
2023-07-31 16:50:29 -0700

    Bugfix: Unirest bug, downgraded to RC1 version. Started importing JSON data

--- src/main/java/com/cfVanguardBackend/oldskoolbackend/services/VanguardAPI.java
+++ src/main/java/com/cfVanguardBackend/oldskoolbackend/services/VanguardAPI.java
@@ -1,14 +1,17 @@
 package com.cfVanguardBackend.oldskoolbackend.services;
 
 import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
 
 import org.springframework.stereotype.Service;
 
 import com.cfVanguardBackend.oldskoolbackend.models.Card;
+import com.cfVanguardBackend.oldskoolbackend.models.Card2;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
+import kong.unirest.UnirestParsingException;
 
 @Service
 public class VanguardAPI {
@@ -33,12 +36,42 @@ public class VanguardAPI {
   //     .asObject(Card.class)
   //     .getBody();
   // }
-  public Card getCard(String id){
+  public Card fetchCard(String id){
     String url = "https://card-fight-vanguard-api.ue.r.appspot.com/api/v1/card?id=" + id;
     System.out.println(url);
-    Card response = Unirest.get(url)
+    Card card = Unirest.get(url)
       .asObject(Card.class)
       .getBody();
-      return response;
+      return card;
+    // try {
+    //   System.out.println(Unirest.get(url)
+    //   .asObject(Card.class)
+    //   // .getParsingError()
+    //   // .get().getMessage());
+    //   .getBody());
+    //   // System.out.println(response);
+    // }
+    // catch (Exception e) {
+    //   System.out.println(e);
+    // }
+    // return response;
+
+    // ----------------------------------------------------------------------------------
+    // HttpResponse<JsonNode> jsonResponse = (HttpResponse<JsonNode>) Unirest.get(url)
+    //   .asJson();
+    // System.out.println(jsonResponse);
+    // String url = "https://card-fight-vanguard-api.ue.r.appspot.com/api/v1/card?id=" + id;
+    // System.out.println(url);
+    // CompletableFuture<HttpResponse<JsonNode>> future = Unirest.get(url)
+    //   .asJsonAsync((response) -> {
+    //     System.out.println(response.getBody());
+    //   });
+
+    // --------------------------------------------------------------------------------
+    //BASIC TEST
+    // System.out.println(Unirest.get(url)
+    //   .asString()
+    //   .getBody());
+
   }
 }
