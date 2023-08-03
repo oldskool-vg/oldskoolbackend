a37db094f33a41405a3f03e374e74ea8ed003426
Rachel Miller <rkmiller131@gmail.com>
2023-08-03 10:55:00 -0700

    Bugfix - card from API response/object mapping can now save to Mongo

--- src/main/java/com/cfVanguardBackend/oldskoolbackend/services/CardService.java
+++ src/main/java/com/cfVanguardBackend/oldskoolbackend/services/CardService.java
@@ -3,7 +3,6 @@ package com.cfVanguardBackend.oldskoolbackend.services;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Future;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,21 +24,16 @@ public class CardService {
     return cardRepository.findAll();
   }
 
-  // public Card oneCard(String name) {
-  //   return cardRepository.findByName(name).orElseThrow(() -> new CardNotFoundException(name)));
-  // }
-
   public Optional<Card> oneCard(String name) {
+    // return cardRepository.findByName(name).orElseThrow(() -> new CardNotFoundException(name)));
     return cardRepository.findByName(name);
   }
 
-  // have a method on our Card service that, when we want to add a new card, will use the api class to get the card from the external api and then save it to our database.
+  // have a method on our Card service that, when we want to add a new card, will use the api class to get the card from the external api and then save it to our database asynchronously.
   public void addCard(String id) {
-    // cardRepository.save(vanguardAPI.getCard(id));
 
     CompletableFuture<Card> future = CompletableFuture.supplyAsync(() -> vanguardAPI.fetchCard(id));
-    future.thenAccept(card -> System.out.println(card));
-    // vanguardAPI.fetchCard(id);
+    future.thenAccept(card -> cardRepository.save(card));
 
   }
 
